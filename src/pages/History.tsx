e046d7a295209dcf146a14d4fc6890f7f31729d0
Jhams <jhamsteak019@gmail.com>
2026-05-30 14:26:42 +0800

    Use month label when no date out is picked

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1770,7 +1770,7 @@ const History = () => {
           initialMonth={selectedMonth.toString()}
           initialYear={selectedYear.toString()}
           dateOutDates={selectedDateOutDates}
-          periodLabel={selectedDateOutLabel || undefined}
+          periodLabel={historyExportPeriodLabel}
         />
       )}
     </div>
